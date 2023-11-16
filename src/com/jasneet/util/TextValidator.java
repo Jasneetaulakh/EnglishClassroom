f2e1d3787119685874fec2527bcdf9cf008e4c23
jasneet <jasneetaulakh@hotmail.com>
2023-11-16 11:32:01 +0000

    simplified isTextValid

--- src/com/jasneet/util/TextValidator.java
+++ src/com/jasneet/util/TextValidator.java
@@ -3,11 +3,7 @@ package com.jasneet.util;
 public class TextValidator {
 
     public static boolean isTextValid(String text) {
-        boolean isValid = true;
-        if(text.isEmpty()) {
-            isValid = false;
-        }
-        return isValid;
+        return(!text.isEmpty());
     }
 
 }
